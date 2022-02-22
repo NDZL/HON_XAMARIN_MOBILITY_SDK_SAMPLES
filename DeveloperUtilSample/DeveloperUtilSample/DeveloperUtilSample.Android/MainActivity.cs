993a4f8dfc537a4202fd140ff05ec81472704afe
{ N.DZL } <NDZL@users.noreply.github.com>
2022-02-22 16:23:42 +0100

    Add files via upload

---
+++ DeveloperUtilSample/DeveloperUtilSample/DeveloperUtilSample.Android/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content.Res;
+
+
+namespace DeveloperUtilSample.Droid
+{
+    [Activity(Label = "DeveloperUtilSample", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            //lock application orientation
+            ScreenLayout screenSize = Application.Context.ApplicationContext.Resources.Configuration.ScreenLayout & ScreenLayout.SizeMask;
+            switch (screenSize)
+            {
+                case ScreenLayout.SizeXlarge:
+                case ScreenLayout.SizeLarge:
+                    this.RequestedOrientation = ScreenOrientation.Landscape;
+                    break;
+                default:
+                    this.RequestedOrientation = ScreenOrientation.Portrait;
+                    break;
+            }
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        protected override async void OnResume()
+        {
+            base.OnResume();
+
+            //Gets the instance of DeveloperUtil
+            bool status = await MainPage.GetDeveloperUtil();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            MainPage.CurrentMainPage.ReleaseDeveloperUtil();
+            MainPage.CurrentMainPage = null;
+
+        }
+    }
+}
+
---
+++ DeveloperUtilSample/DeveloperUtilSample/DeveloperUtilSample/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Honeywell.AIDC.Droid.OS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DeveloperUtilSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public static MainPage CurrentMainPage { get; set; } = null;
+        public static DeveloperUtil CurrentDeveloperUtil { get; set; } = null;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            CurrentMainPage = this;
+        }
+
+        public static async Task<bool> GetDeveloperUtil()
+        {
+            bool status = true;
+            try
+            {
+                if (null == CurrentDeveloperUtil)
+                {
+                    CurrentDeveloperUtil = await DeveloperUtil.GetInstance();
+                }
+            }
+            catch (HonOSException exp)
+            {
+                CurrentMainPage.DisplayStatus("HonOSException: " + exp.Message);
+            }
+
+            return status;
+        }
+
+        private void ButtonClear_Clicked(object sender, EventArgs e)
+        {
+            EditorStatus.Text = string.Empty;
+        }
+
+        public void ReleaseDeveloperUtil()
+        {
+            if (null != CurrentDeveloperUtil)
+            {
+                CurrentDeveloperUtil.Dispose();
+                CurrentDeveloperUtil = null;
+            }
+        }
+
+        private void DisplayStatus(string info)
+        {
+            if (EditorStatus.Text == string.Empty)
+            {
+                EditorStatus.Text = info;
+            }
+            else
+            {
+                EditorStatus.Text += Environment.NewLine + info;
+            }
+        }
+
+        private void ButtonSelfDiagnostic_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (null != CurrentDeveloperUtil)
+                {
+                    CurrentDeveloperUtil.ActivateSelfDiagnostic();
+                    CurrentMainPage.DisplayStatus("Activated self diagnostic.");
+                }
+            }
+            catch (HonOSException exp)
+            {
+                CurrentMainPage.DisplayStatus("HonOSException: " + exp.Message);
+            }
+        }
+    }
+}
